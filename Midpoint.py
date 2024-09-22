77e2c1c89557b4811a977ad3d7552ac3e0ea757e
nadiya-2003 <154906455+nadiya-2003@users.noreply.github.com>
2024-09-22 16:52:41 +0530

    Midpoint.py

---
+++ Midpoint.py
@@ -0,0 +1,7 @@
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+mid_x = (x1 + x2) / 2
+mid_y = (y1 + y2) / 2
+print(f"Binoy's house is located at ({mid_x:.1f},{mid_y:.1f})")
